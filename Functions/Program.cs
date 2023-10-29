e391ba2d0e77173553224ec0c3233b74437aad1f
Markus <Markus@20111759-D>
2023-10-29 19:14:03 +0200

    Add project files.

---
+++ Functions/Program.cs
@@ -0,0 +1,14 @@
+﻿
+PrintHello()
+PrintGoodBye()
+
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you late alligator!");
+}
---
+++ test/Program.cs
@@ -0,0 +1,32 @@
+﻿ Console.WriteLine("Enter the temperature:");
+double userTemperature = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Do you want to convert to Celsius or Fahrenheit?");
+string userInput = Console.ReadLine();
+
+if (userInput.Equals("Celsius", StringComparison.OrdinalIgnoreCase))
+        {
+            ConvertToFahrenheit(userTemperature);
+        }
+else if (userInput.Equals("Fahrenheit", StringComparison.OrdinalIgnoreCase))
+        {
+            ConvertToCelsius(userTemperature);
+        }
+else
+        {
+            Console.WriteLine("Invalid choice. Please select Celsius or Fahrenheit.");
+        }
+
+
+static void ConvertToFahrenheit(double celsius)
+    {
+        double fahrenheit = (celsius * 9) / 5 + 32;
+        Console.WriteLine($"Fahrenheit: {fahrenheit:0}°F");
+    }
+
+static void ConvertToCelsius(double fahrenheit)
+    {
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        Console.WriteLine($"Celsius: {celsius:0}°C");
+    }
+
